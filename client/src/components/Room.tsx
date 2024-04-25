f349c963f6ed301c326f27eabbf7d51ca72b16f5
Aditya Raj <adistrim.dev@gmail.com>
2024-04-25 17:26:13 +0530

    getting to the basic functionality

--- client/src/components/Room.tsx
+++ client/src/components/Room.tsx
@@ -1,19 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom"
+import { Socket, io } from "socket.io-client";
 
+const URL = "http://localhost:3000"
 
 export const Room = () => {
-    // const [searchParams, setSearchParams] = useSearchParams();
-    const [searchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
+    // const [searchParams] = useSearchParams();
     const name = searchParams.get("name");
+    const [socket, setSocket] = useState<null | Socket>(null);
+    const [lobby, setLobby] = useState(true);
+
 
     useEffect(() => {
         // logic to join room
+        const socket = io(URL)
+        socket.on("send-offer", ({roomId}) => {
+            alert("send offer, please.")
+            setLobby(false)
+            socket.emit("offer", {
+                sdp: "",
+                roomId
+            })
+        })
+        socket.on("offer", ({roomId, offer}) => {
+            alert("send answer, please.")
+            setLobby(false)
+            socket.emit("answer", {
+                roomId,
+                sdp: ""
+            })
+        })
+        socket.on("answer", ({roomId, answer}) => {
+            setLobby(false)
+            alert("connection done.")
+        })
+        socket.on("lobby", () => {
+            setLobby(true)
+        })
+        setSocket(socket)
     }, [name])
+
+    if (lobby) {
+        return (
+            <div>
+                <h1>Waiting to connect you to someone...</h1>
+            </div>
+        )
+    }
 
     return (
         <div>
             <h1>Hi, {name}</h1>
+            <video width={400}></video>
+            <video width={400}></video>
         </div>
     )
 }
